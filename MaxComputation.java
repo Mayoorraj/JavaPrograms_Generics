0ce14c308d0ae4b730b99e2d930a36b86d6cbd12
Mayoor <mayoordoiphode@gmail.com>
2022-05-03 12:29:35 +0530

    Added: msg for Max computation program

---
+++ MaxComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.assignment13;
+
+public class MaxComputation {
+
+    public static void main(String[] args) {
+
+        System.out.println("----- Computing Maximum Of Three Numbers using Java Generics ------\n");
+    }
+}
